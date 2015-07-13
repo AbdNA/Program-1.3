cce55f9ab7c584c555620df92adf13c16933b729
AbdNA <idied74@gmail.com>
2015-07-13 13:48:48 +0600

    Code

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число в двоичной для его перевода в восьмеричную, десятичную, шестнадцатеричную системы соответственно:");
+      
+
+            string a = Console.ReadLine();
+
+            Console.WriteLine(Convert.ToString(Convert.ToInt32(a, 2), 8));
+            Console.WriteLine(Convert.ToString(Convert.ToInt32(a, 2), 10));
+            Console.WriteLine(Convert.ToString(Convert.ToInt32(a, 2), 16));
+            
+            Console.ReadKey();
+        }
+    }
+}
